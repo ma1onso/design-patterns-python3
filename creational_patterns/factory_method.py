d36d736e844fd67bc27af209fe0880b97b15e482
Alonso <alons45@gmail.com>
2024-05-21 14:53:16 -0500

    Create factory_method.py

---
+++ creational_patterns/factory_method.py
@@ -0,0 +1,59 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+
+
+# Base class
+class Logistic(ABC):
+    @abstractmethod
+    def create_transport(self):
+        pass
+
+    def delivery_plan(self) -> str:
+        transport  = self.create_transport()
+
+        result = f"Shipping the packages {transport.deliver()}"
+        
+        return result
+    
+
+class RoadLogistic(Logistic):
+    def create_transport(self) -> Transport:
+        return Truck()
+
+
+class SeaLogistic(Logistic):
+    def create_transport(self) -> Transport:
+        return Ship()
+
+
+# Base class
+class Transport(ABC):
+    @abstractmethod
+    def deliver(self) -> str:
+        pass
+    
+
+class Truck(Transport):
+    def deliver(self) -> str:
+        return "Driving a Tesla Truck"
+    
+
+class Ship(Transport):
+    def deliver(self) -> str:
+        return "Sailing the ocean"
+
+
+def client_code(logistic_creator: Logistic) -> None:
+    print(
+        f"Client: I'm not aware of the logistic creator's class, but it still works.\n"
+        f"{logistic_creator.delivery_plan()}", end=""
+    )
+
+if __name__ == "__main__":
+    print("App: Launched with the RoadLogistic.")
+    client_code(RoadLogistic())
+    
+    print("\n")
+    
+    print("App: Launched with the SeaLogistic.")
+    client_code(SeaLogistic())
