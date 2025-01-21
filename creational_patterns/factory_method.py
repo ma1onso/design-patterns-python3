4856a8aa6eaa1c8838249b4c1f3239d5c6bd2ea9
Alonso <alons45@gmail.com>
2025-01-21 16:59:38 -0500

    Add a short description factory_method.py

--- creational_patterns/factory_method.py
+++ creational_patterns/factory_method.py
@@ -1,3 +1,5 @@
+# Factory Method is a creational design pattern that provides an interface for creating objects in a superclass, but allows subclasses to alter the type of objects that will be created.
+
 from __future__ import annotations
 from abc import ABC, abstractmethod
 
